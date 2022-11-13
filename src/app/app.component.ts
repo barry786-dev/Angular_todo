5d809e6c7ce0938b5b5ef83b1d2faee9f547e28d
barry <mbrsyr@yahoo.com>
2022-11-13 08:36:19 +0100

    angular to do

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
 
+
+import { Todo } from './todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'firstAngularApp';
+  title = 'firstAngularApp project';
+  todoValue!: string;
+  list: Todo[] = [];
+
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = '';
+  }
+  // inputChangeHandler(event: any) {
+  //   this.todoValue = event.target.value;
+  // }
+  addItem() {
+    console.log(this.todoValue);
+
+    if (this.todoValue !== '') {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false,
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = '';
+    console.log(this.list);
+  }
+
+  // toggleEditable(e: any, id: number) {
+  //   if (e.target.checked) {
+  //     this.list = this.list.map((item) => {
+  //       if (item.id === id) {
+  //         return {
+  //           ...item,
+  //           isDone: true,
+  //         };
+  //       }
+  //       return item;
+  //     });
+  //   } else {
+  //     this.list = this.list.map((item) => {
+  //       if (item.id === id) {
+  //         return {
+  //           ...item,
+  //           isDone: false,
+  //         };
+  //       }
+  //       return item;
+  //     });
+  //   }
+  //   console.log(this.list);
+    
+  // }
+
+  deleteItem(id: number) {
+    this.list = this.list.filter((item) => item.id !== id);
+  }
 }
